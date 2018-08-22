8a1e7915a414742315373f1df7f1f9911b844450
battulakarthik <41747943+battulakarthik@users.noreply.github.com>
2018-08-22 19:54:27 +0530

    Update 16.py

--- 16.py
+++ 16.py
@@ -1,12 +1,10 @@
 n=int(input())
 lst=[int(x) for x in input().split()]
-c=sorted(lst)
-candy1=[]
 candy=n
 for i in range(0,n-1):
    for j in range(i+1,n):
-       if c[i]==c[j]:
+       if lst[i]==lst[j]:
            break
-       elif c[i]<c[j]:
+       elif lst[i]<lst[j]:
            candy+=1
 print(candy)
