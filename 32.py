19e4e10324ef65a5f5dd997dbaa43c8367a5f1c0
battulakarthik <41747943+battulakarthik@users.noreply.github.com>
2018-09-18 15:16:10 +0530

    Update 32.py

--- 32.py
+++ 32.py
@@ -5,14 +5,14 @@ for x in range(0,n):
     mx.append(sorted(row))
 clmx=[]
 mx=tuple(mx)
-for i in range(0,n):
+for i in range(0,m):
     cl=[]
-    for j in range(0,m):
+    for j in range(0,n):
         cl.append(mx[j][i])
     clmx.append(sorted(cl))
-for i in range(0,m):
-    for j in range(0,n):
-        if j<n-1:k=' '
+for i in range(0,n):
+    for j in range(0,m):
+        if j<m-1:k=' '
         else:k=''
         print(clmx[j][i],end=k)
     print()
